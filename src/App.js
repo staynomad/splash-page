dade0cdeb4307f1fe1292290e49756a9eefad69b
MikePodo <72575260+MikePodo@users.noreply.github.com>
2021-06-16 19:48:45 -0700

    create live countdown timer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./app.css";
 
 const App = () => {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    setInterval(countdown, 1000);
+    return () => {
+      clearInterval(countdown);
+    };
+  }, []);
+
+  const countdown = () => {
+    //Get date info
+    const sep1 = new Date("September 1, 2021 00:00:00").getTime();
+    const currentDate = new Date().getTime();
+    const gap = sep1 - currentDate;
+
+    //Calculations
+    const s = 1000;
+    const m = s * 60;
+    const h = m * 60;
+    const d = h * 60;
+
+    //setState
+    setDays(Math.floor(gap / d));
+    setHours(Math.floor((gap % d) / h));
+    setMinutes(Math.floor((gap % h) / m));
+    setSeconds(Math.floor((gap % m) / s));
+  };
+
   const handleOpenSocial = (href) => {
     window.open(href, "_blank");
   };
@@ -62,28 +93,28 @@ const App = () => {
         <h2 className="subtitle">The future of flexible rentals</h2>
         <div className="timer-container">
           <div className="timer-number">
-            <h3 className="timer">00</h3>
+            <h3 className="timer">{days}</h3>
             <div className="timer-label">
               <h5>Days</h5>
             </div>
           </div>
           <h4 className="timer">:</h4>
           <div className="timer-number">
-            <h3 className="timer">00</h3>
+            <h3 className="timer">{hours}</h3>
             <div className="timer-label">
               <h5>Hours</h5>
             </div>
           </div>
           <h4 className="timer">:</h4>
           <div className="timer-number">
-            <h3 className="timer">00</h3>
+            <h3 className="timer">{minutes}</h3>
             <div className="timer-label">
               <h5>Minutes</h5>
             </div>
           </div>
           <h4 className="timer">:</h4>
           <div className="timer-number">
-            <h3 className="timer">00</h3>
+            <h3 className="timer">{seconds}</h3>
             <div className="timer-label">
               <h5>Seconds</h5>
             </div>
